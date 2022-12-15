b79fbfdc565b9d9fa1ec5b23f3075ed702aa3414
shubhamsigdar1 <shubhamsigdar@gmail.com>
2022-12-15 12:00:07 +0530

    wordle change after discussion

--- script.js
+++ script.js
@@ -26,33 +26,10 @@ function initBoard(){
 // run function automatically
 initBoard();
 
-//  Accept User Input(checking valid input or not)
-// document.addEventListener("keyup",(event)=>{
-//     console.log(event);
-//     if(guessesRemaining===0){
-//         return;
-//     }
-//     let pressedKey = String(event.key);
-//     // hit backspace and if row is not empty
-//     if(pressedKey==="Backspace" && nextLetter!==0){
-//         deleteLetter();
-//     }
-//     if(pressedKey==="Enter"){
-//         checkGuess();
-//         return;
-//     }
-
-//     let found=pressedKey.match(/a-z/gi);
-//     if(!found || found.length>1){
-//         return;
-//     }else{
-//         insertLetter(pressedKey);
-//     }
-// })
 document.addEventListener("keyup", (e) => {
-
+    // console.log('event',e);
     if (guessesRemaining === 0) {
-        return
+        return;
     }
 
     let pressedKey = String(e.key)
@@ -67,6 +44,7 @@ document.addEventListener("keyup", (e) => {
     }
 
     let found = pressedKey.match(/[a-z]/gi)
+    // console.log('found',found)
     if (!found || found.length > 1) {
         return
     } else {
@@ -75,35 +53,18 @@ document.addEventListener("keyup", (e) => {
 })
 
 
-// inserLetter ( insertLetter(pressedKey);)
-// function insertLetter(pressedKey){
-//     if(nextLetter===5){
-//         return;
-//     }
-//     pressedKey=pressedKey.toLowerCase();
-    
-//     // identofy correct row
-//     let row = document.getElementsByClassName("letter-row")[6-guessesRemaining];
-//     let box = row.children[nextLetter];
-//     box.textContent=pressedKey;
-//     box.classList.add("filled-box");
-//     //adding current pressed key to the end of current guess
-//     currentGuess.push(pressedKey);
-//     //track where we are in the row...i.e. why we increment by 1
-//     nextLetter+=1;
-    
-// }
 function insertLetter (pressedKey) {
     if (nextLetter === 5) {
         return
     }
     pressedKey = pressedKey.toLowerCase()
-
+    // console.log(document.getElementsByClassName("letter-row"))
     let row = document.getElementsByClassName("letter-row")[6 - guessesRemaining]
+    // console.log(row.children)
     let box = row.children[nextLetter]
     box.textContent = pressedKey
     box.classList.add("filled-box")
-    currentGuess.push(pressedKey)
+    currentGuess.push(pressedKey) 
     nextLetter += 1
 }
 
@@ -120,65 +81,6 @@ function deleteLetter(){
 }
 
 
-// function checkGuess(){
-//     let row = document.getElementsByClassName("letter-row")[6-guessesRemaining];
-//     let guessString='';
-//     let rightGuess=Array.from(rightGuessString);
-
-//     for(const val of currentGuess){
-//         guessString+=val;
-//     }
-//     if(guessString.length!=5){
-//         alert("Not enough letters!");
-//         return;
-//     }
-//     if(!WORDS.includes(guessString)){
-//         alert("word not in list");
-//         return;
-//     }
-// wordle game:- If a letter is yellow its a right letter i.e. it exist in the word but it is in the wrong position and If a letter is green its a right letter i.e. it is in the right position
-//     for(let i=0;i<5;i++){
-//         let letterColor = '';
-//         let box = row.children[i];
-//         let letter = currentGuess[i];
-        
-//         let letterPosition = rightGuess.indexOf(currentGuess[i]);
-//         //if we don't get valid words then indexOf return -1;
-//         if(letterPosition===-1){
-//             letterColor='grey';
-//         }else{
-//             if(currentGuess[i]===rightGuess[i]){
-//                 letterColor='green';
-//             }else{
-//                 letterColor='yellow';
-//             }
-
-//             //DOUBT 
-//             rightGuess[letterPosition]="#";
-//         }
-//         //when we hit enter there is some delay
-//         let delay=250*i;
-//         setTimeout(()=>{
-//             box.style.backgroundColor = letterColor;
-//             shadeKeyBoard(letter, letterColor);
-//         },delay)
-//     }
-//     if(guessString===rightGuessString){
-//         alert("You guessed right! Game over");
-//         guessesRemaining=0;
-//         return;
-//     }else{
-//         guessesRemaining-=1;
-//         currentGuess=[];
-//         nextLetter=0;
-
-//         if(guessesRemaining===0){
-//             alert("You've run out of guesses! Game over!");
-//             alert(`The right word was: "${rightGuessString}"`);
-//         }
-//     }
-
-// }
 function checkGuess () {
     let row = document.getElementsByClassName("letter-row")[6 - guessesRemaining]
     let guessString = ''
@@ -193,6 +95,7 @@ function checkGuess () {
         return
     }
 
+
     if (!WORDS.includes(guessString)) {
         alert("Word not in list!")
         return
@@ -203,7 +106,7 @@ function checkGuess () {
         let letterColor = ''
         let box = row.children[i]
         let letter = currentGuess[i]
-        
+
         let letterPosition = rightGuess.indexOf(currentGuess[i])
         // is letter in the correct guess
         if (letterPosition === -1) {
@@ -219,7 +122,7 @@ function checkGuess () {
                 // shade box yellow
                 letterColor = 'yellow'
             }
-
+            
             rightGuess[letterPosition] = "#"
         }
 
@@ -247,27 +150,8 @@ function checkGuess () {
     }
 }
 
-//ShadeKeyboard-it shade greed now we shade keyboard
-// function shadeKeyBoard(letter, color){
-//     //loop through that whole keyboard 
-//     for(const elem of document.getElementsByClassName("kyboard-button")){
-//         //Does the content of that keyboard match that letter
-//         if(elem.textContent===letter){
-//                 let oldColor = elem.style.backgroundColor;
-//                 if(oldColor==='green'){
-//                     //not change green color exit from function
-//                     return;
-//                 }
-//                 if(oldColor==='yellow' && color!='green'){
-//                     return;
-//                 }
-//                 elem.style.backgroundColor=color;
-//                 break;
-            
-//         }
-//     }
-// }
 function shadeKeyBoard(letter, color) {
+    // console.log(document.getElementsByClassName("keyboard-button"))
     for (const elem of document.getElementsByClassName("keyboard-button")) {
         if (elem.textContent === letter) {
             let oldColor = elem.style.backgroundColor
@@ -285,34 +169,17 @@ function shadeKeyBoard(letter, color) {
     }
 }
 
-//To Make the On-screen Keyboard Generate Input(When we click keyboard button the letter get inserted in the grid)
 
-//add eventListener on keyboard button and we are doing function inside the eventListener
-// document.getElementById("Keyboard-cont").addEventListener("click",(e)=>{
-//     const target = e.target;
-//     //if we cannot click keyboard button
-//     if(!target.classList.contains("keyboard-button")){
-//         return;
-//     }
-
-//     let key = target.textContent;
-//     if(key==='Del'){
-//         key="Backspace"
-//     }
-
-//     //we are mimicing the real keyboard with virtual keyboard
-//     document.dispatchEvent(new KeyboardEvent("Keyup",{'key':key}))
-// })
 document.getElementById("keyboard-cont").addEventListener("click", (e) => {
     const target = e.target
-    
+    console.log(e.target)
     if (!target.classList.contains("keyboard-button")) {
         return
     }
     let key = target.textContent
 
     if (key === "Del") {
-        key = "Backspace"
+        key = "Backspace";
     } 
 
     document.dispatchEvent(new KeyboardEvent("keyup", {'key': key}))
